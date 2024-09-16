db678e5ef0b3e0b0c22f4cb3ec0aade2348e508d
庄黛淳华 <miku1958@users.noreply.github.com>
2024-09-16 14:37:01 +0800

    update

--- user.js
+++ user.js
@@ -1,35 +1,33 @@
 // ==UserScript==
-// @name        Katakana Terminator
-// @description Convert gairaigo (Japanese loan words) back to English
-// @author      Arnie97
+// @name        Epipalaeolithic
+// @description A browser extension to add ipa to English word
+// @author      Mi
 // @license     MIT
-// @copyright   2017-2024, Katakana Terminator Contributors (https://github.com/Arnie97/katakana-terminator/graphs/contributors)
-// @namespace   https://github.com/Arnie97
-// @homepageURL https://github.com/Arnie97/katakana-terminator
-// @supportURL  https://greasyfork.org/scripts/33268/feedback
+// @namespace   https://github.com/miku1958
+// @homepageURL https://github.com/miku1958/Epipalaeolithic
 // @icon        https://upload.wikimedia.org/wikipedia/commons/2/28/Ja-Ruby.png
 // @match       *://*/*
-// @exclude     *://*.bilibili.com/video/*
 // @grant       GM.xmlHttpRequest
 // @grant       GM_xmlhttpRequest
 // @grant       GM_addStyle
-// @connect     translate.google.cn
-// @connect     translate.google.com
-// @connect     translate.googleapis.com
-// @version     2024.05.05
-// @name:ja-JP  カタカナターミネーター
-// @name:zh-CN  片假名终结者
-// @description:zh-CN 在网页中的日语外来语上方标注英文原词
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @connect     cn.bing.com
+// @version     2024.09.14
+// @downloadURL https://raw.githubusercontent.com/miku1958/Epipalaeolithic/master/user.js
+// @updateURL   https://raw.githubusercontent.com/miku1958/Epipalaeolithic/master/user.js
 // ==/UserScript==
 
 // define some shorthands
 var _ = document;
-
-var queue = {};  // {"カタカナ": [rtNodeA, rtNodeB]}
-var cachedTranslations = {};  // {"ターミネーター": "Terminator"}
+const queue = {}; // {"community": [rtNodeA, rtNodeB]}
 var newNodes = [_.body];
 
+/** @type { Element[] } */
+let skipElements = [];
+
 // Recursively traverse the given node and its descendants (Depth-first search)
+/** @param { Node } node */
 function scanTextNodes(node) {
     // The node could have been detached from the DOM tree
     if (!node.parentNode || !_.body.contains(node)) {
@@ -37,34 +35,114 @@ function scanTextNodes(node) {
     }
 
     // Ignore text boxes and echoes
-    var excludeTags = {ruby: true, script: true, select: true, textarea: true};
+    var excludeTags = {
+        ruby: true,
+        script: true,
+        select: true,
+        textarea: true,
+        style: true,
+        code: true,
+        button: true,
+        a: true,
+    };
+    var excludeRole = { table: true };
+    var excludeAriaLabel = { chats: true };
+    var excludeClass = [
+        "ui-card__body", // Teams calendar card
+        "code-container", // greasyfork.org code
+        "diff-table", // github.com code diff
+    ];
+    var excludeDataTrackActionScenario = { messageQuotedReplyDeeplink: true };
 
-    switch (node.nodeType) {
-    case Node.ELEMENT_NODE:
-        if (node.tagName.toLowerCase() in excludeTags || node.isContentEditable) {
+    // if node is subnode element of skipElements, return
+    for (let skipElement of skipElements) {
+        if (skipElement.contains(node)) {
             return;
         }
-        return Array.from(node.childNodes).forEach(scanTextNodes);
+    }
+    switch (node.nodeType) {
+        case Node.ELEMENT_NODE:
+            /** @type { Element } */
+            const element = node;
+            if (
+                element.tagName.toLowerCase() in excludeTags ||
+                element.isContentEditable ||
+                element.role?.toLowerCase() in excludeRole ||
+                element.ariaLabel?.toLowerCase() in excludeAriaLabel ||
+                element.dataset.trackActionScenario in excludeDataTrackActionScenario
+            ) {
+                skipElements.push(element);
+                return;
+            }
+            for (const class_ of excludeClass) {
+                if (element.classList.contains(class_)) {
+                    skipElements.push(element);
+                    return;
+                }
+            }
 
-    case Node.TEXT_NODE:
-        while ((node = addRuby(node)));
+            if (element.computedStyleMap().get("height") != "auto") {
+                return;
+            }
+
+            for (let i = element.childNodes.length - 1; i >= 0; i--) {
+                scanTextNodes(element.childNodes[i]);
+            }
+        case Node.TEXT_NODE:
+            let paragraph = node.parentElement;
+            let hasRuby = false;
+            while ((node = addRuby(node))) {
+                hasRuby = true;
+            }
+            if (hasRuby) {
+                paragraph.style.alignItems = "end";
+                const currentLineHeight = paragraph
+                    .computedStyleMap()
+                    .get("line-height");
+                if (currentLineHeight.unit == "number") {
+                    paragraph.style.lineHeight = `max(${currentLineHeight.value * 100
+                        }%, min(300%, 30pt))`;
+                } else {
+                    paragraph.style.lineHeight = "min(300%, 30pt)";
+                }
+                while (true) {
+                    if (paragraph == null) {
+                        break;
+                    }
+                    if (paragraph.style.webkitLineClamp != "") {
+                        // Fix Safari bug
+                        paragraph.style.webkitLineClamp = "unset";
+                    }
+                    if (paragraph.tagName === "DIV") {
+                        break;
+                    }
+                    paragraph = paragraph.parentElement;
+                }
+            }
     }
 }
 
 // Recursively add ruby tags to text nodes
 // Inspired by http://www.the-art-of-web.com/javascript/search-highlight/
+/**
+ * @param { Text } node
+ * @return { Node | false }
+ */
 function addRuby(node) {
-    var katakana = /[\u30A1-\u30FA\u30FD-\u30FF][\u3099\u309A\u30A1-\u30FF]*[\u3099\u309A\u30A1-\u30FA\u30FC-\u30FF]|[\uFF66-\uFF6F\uFF71-\uFF9D][\uFF65-\uFF9F]*[\uFF66-\uFF9F]/, match;
-    if (!node.nodeValue || !(match = katakana.exec(node.nodeValue))) {
+    var word = /[a-zA-Z]{2,}/,
+        match;
+    if (!node.nodeValue || !(match = word.exec(node.nodeValue))) {
         return false;
     }
-    var ruby = _.createElement('ruby');
+
+    var ruby = _.createElement("ruby");
     ruby.appendChild(_.createTextNode(match[0]));
-    var rt = _.createElement('rt');
-    rt.classList.add('katakana-terminator-rt');
+
+    var rt = _.createElement("rt");
+    rt.classList.add("ipa-additional-rt");
     ruby.appendChild(rt);
 
-    // Append the ruby title node to the pending-translation queue
+    // Append the ruby title node to the pending-query queue
     queue[match[0]] = queue[match[0]] || [];
     queue[match[0]].push(rt);
 
@@ -80,168 +158,116 @@ function addRuby(node) {
 function translateTextNodes() {
     var apiRequestCount = 0;
     var phraseCount = 0;
-    var chunkSize = 200;
-    var chunk = [];
 
     for (var phrase in queue) {
         phraseCount++;
-        if (phrase in cachedTranslations) {
-            updateRubyByCachedTranslations(phrase);
+        let cache = GM_getValue(phrase.toLowerCase(), null);
+        if (cache !== null) {
+            updateRuby(phrase, cache);
             continue;
         }
 
-        chunk.push(phrase);
-        if (chunk.length >= chunkSize) {
-            apiRequestCount++;
-            translate(chunk, apiList);
-            chunk = [];
-        }
-    }
-
-    if (chunk.length) {
         apiRequestCount++;
-        translate(chunk, apiList);
+        bingIPAForPhrase(phrase);
     }
 
     if (phraseCount) {
-        console.debug('Katakana Terminator:', phraseCount, 'phrases translated in', apiRequestCount, 'requests, frame', window.location.href);
+        console.debug(
+            "IPA Additional:",
+            phraseCount,
+            "phrases translated in",
+            apiRequestCount,
+            "requests, frame",
+            window.location.href
+        );
     }
 }
 
-// {"keyA": 1, "keyB": 2} => "?keyA=1&keyB=2"
+/**
+ * {"keyA": 1, "keyB": 2} => "?keyA=1&keyB=2"
+ *
+ * @param { string } params
+ * @return {*}
+ */
 function buildQueryString(params) {
-    return '?' + Object.keys(params).map(function(k) {
-        return encodeURIComponent(k) + '=' + encodeURIComponent(params[k]);
-    }).join('&');
+    return (
+        "?" +
+        Object.keys(params)
+            .map(function (k) {
+                return encodeURIComponent(k) + "=" + encodeURIComponent(params[k]);
+            })
+            .join("&")
+    );
 }
+/**
+ *
+ *
+ * @param { string } phrase
+ */
+function bingIPAForPhrase(phrase) {
+    // https://cn.bing.com/api/v7/dictionarywords/search?q=community&appid=371E7B2AF0F9B84EC491D731DF90A55719C7D209&mkt=zh-cn
+    var api = "https://cn.bing.com/api/v7/dictionarywords/search",
+        params = {
+            q: phrase,
+            appid: "371E7B2AF0F9B84EC491D731DF90A55719C7D209",
+            mkt: "zh-cn",
+        };
 
-function translate(phrases) {
-    if (!apiList.length) {
-        console.error('Katakana Terminator: fallbacks exhausted', phrases);
-        phrases.forEach(function(phrase) {
-            delete cachedTranslations[phrase];
-        });
-    }
-
-    // Prevent duplicate HTTP requests before the request completes
-    phrases.forEach(function(phrase) {
-        cachedTranslations[phrase] = null;
-    });
-
-    var api = apiList[0];
     GM_xmlhttpRequest({
         method: "GET",
-        url: 'https://' + api.hosts[0] + api.path + buildQueryString(api.params(phrases)),
-        onload: function(dom) {
+        url: api + buildQueryString(params),
+        onload: function (dom) {
             try {
-                api.callback(phrases, JSON.parse(dom.responseText.replace("'", '\u2019')));
+                var resp = JSON.parse(dom.responseText.replace("'", "\u2019"));
             } catch (err) {
-                console.error('Katakana Terminator: invalid response', err, dom.responseText);
-                apiList.shift();
-                return translate(phrases);
+                console.error("IPA Additional: invalid response", dom.responseText);
+                return;
             }
+            let pronunciation = "";
+            if (resp.value[0] != null && resp.value[0]?.pronunciation != null) {
+                pronunciation = resp.value[0].pronunciation.replace(/[()]/g, "");
+            }
+            GM_setValue(phrase.toLowerCase(), pronunciation);
+            updateRuby(phrase, pronunciation);
         },
-        onerror: function() {
-            console.error('Katakana Terminator: request error', api.url);
-            apiList.shift();
-            return translate(phrases);
+        onerror: function (dom) {
+            console.error("IPA Additional: request error", dom.statusText);
         },
     });
 }
 
-var apiList = [
-    {
-        // https://github.com/Arnie97/katakana-terminator/pull/8
-        name: 'Google Translate',
-        hosts: ['translate.googleapis.com'],
-        path: '/translate_a/single',
-        params: function(phrases) {
-            var joinedText = phrases.join('\n').replace(/\s+$/, '');
-            return {
-                sl: 'ja',
-                tl: 'en',
-                dt: 't',
-                client: 'gtx',
-                q: joinedText,
-            };
-        },
-        callback: function(phrases, resp) {
-            resp[0].forEach(function(item) {
-                var translated = item[0].replace(/\s+$/, ''),
-                    original   = item[1].replace(/\s+$/, '');
-                cachedTranslations[original] = translated;
-                updateRubyByCachedTranslations(original);
-            });
-        },
-    },
-    {
-        // https://github.com/ssut/py-googletrans/issues/268
-        name: 'Google Dictionary',
-        hosts: ['translate.google.cn'],
-        path: '/translate_a/t',
-        params: function(phrases) {
-            var joinedText = phrases.join('\n').replace(/\s+$/, '');
-            return {
-                sl: 'ja',
-                tl: 'en',
-                dt: 't',
-                client: 'dict-chrome-ex',
-                q: joinedText,
-            };
-        },
-        callback: function(phrases, resp) {
-            // ["katakana\nterminator"]
-            if (!resp.sentences) {
-                var translated = resp[0].split('\n');
-                if (translated.length !== phrases.length) {
-                    throw [phrases, resp];
-                }
-                translated.forEach(function(trans, i) {
-                    var orig = phrases[i];
-                    cachedTranslations[orig] = trans;
-                    updateRubyByCachedTranslations(orig);
-                });
-                return;
-            }
-
-            resp.sentences.forEach(function(s) {
-                if (!s.orig) {
-                    return;
-                }
-                var original = s.orig.trim(),
-                    translated = s.trans.trim();
-                cachedTranslations[original] = translated;
-                updateRubyByCachedTranslations(original);
-            });
-        },
-    },
-];
-
-// Clear the pending-translation queue
-function updateRubyByCachedTranslations(phrase) {
-    if (!cachedTranslations[phrase]) {
-        return;
+// Clear the pending-query queue
+/**
+ *
+ *
+ * @param { string } phrase
+ * @param { string } ipa
+ */
+function updateRuby(phrase, ipa) {
+    if (ipa !== "") {
+        (queue[phrase] || []).forEach(function (node) {
+            node.dataset.rt = ipa;
+        });
     }
-    (queue[phrase] || []).forEach(function(node) {
-        node.dataset.rt = cachedTranslations[phrase];
-    });
     delete queue[phrase];
 }
 
 // Watch newly added DOM nodes, and save them for later use
 function mutationHandler(mutationList) {
-    mutationList.forEach(function(mutationRecord) {
-        mutationRecord.addedNodes.forEach(function(node) {
+    mutationList.forEach(function (mutationRecord) {
+        mutationRecord.addedNodes.forEach(function (node) {
             newNodes.push(node);
         });
     });
 }
 
 function main() {
-    GM_addStyle("rt.katakana-terminator-rt::before { content: attr(data-rt); }");
+    GM_addStyle(
+        "rt.ipa-additional-rt::before { content: attr(data-rt); font-size: clamp(10pt, 5vw, 70%); opacity: 0.6; }"
+    );
 
     var observer = new MutationObserver(mutationHandler);
-    observer.observe(_.body, {childList: true, subtree: true});
+    observer.observe(_.body, { childList: true, subtree: true });
 
     function rescanTextNodes() {
         // Deplete buffered mutations
@@ -250,32 +276,39 @@ function main() {
             return;
         }
 
-        console.debug('Katakana Terminator:', newNodes.length, 'new nodes were added, frame', window.location.href);
+        console.debug(
+            "IPA Additional:",
+            newNodes.length,
+            "new nodes were added, frame",
+            window.location.href
+        );
         newNodes.forEach(scanTextNodes);
         newNodes.length = 0;
         translateTextNodes();
     }
 
     // Limit the frequency of API requests
-    rescanTextNodes();
-    setInterval(rescanTextNodes, 500);
+    setInterval(rescanTextNodes, 1000);
 }
 
 // Polyfill for Greasemonkey 4
-if (typeof GM_xmlhttpRequest === 'undefined' &&
-    typeof GM === 'object' && typeof GM.xmlHttpRequest === 'function') {
+if (
+    typeof GM_xmlhttpRequest === "undefined" &&
+    typeof GM === "object" &&
+    typeof GM.xmlHttpRequest === "function"
+) {
     GM_xmlhttpRequest = GM.xmlHttpRequest;
 }
 
-if (typeof GM_addStyle === 'undefined') {
-    GM_addStyle = function(css) {
-        var head = _.getElementsByTagName('head')[0];
+if (typeof GM_addStyle === "undefined") {
+    GM_addStyle = function (css) {
+        var head = _.getElementsByTagName("head")[0];
         if (!head) {
             return null;
         }
 
-        var style = _.createElement('style');
-        style.setAttribute('type', 'text/css');
+        var style = _.createElement("style");
+        style.setAttribute("type", "text/css");
         style.textContent = css;
         head.appendChild(style);
         return style;
@@ -283,8 +316,8 @@ if (typeof GM_addStyle === 'undefined') {
 }
 
 // Polyfill for ES5
-if (typeof NodeList.prototype.forEach === 'undefined') {
-    NodeList.prototype.forEach = function(callback, thisArg) {
+if (typeof NodeList.prototype.forEach === "undefined") {
+    NodeList.prototype.forEach = function (callback, thisArg) {
         thisArg = thisArg || window;
         for (var i = 0; i < this.length; i++) {
             callback.call(thisArg, this[i], i, this);
